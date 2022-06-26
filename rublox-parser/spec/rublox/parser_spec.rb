75899af32f3728993b79d2c4eac3b778ed3cd010
Zach Thomae <zach@thomae.co>
2022-06-26 10:18:12 -0500

    Add basic snapshot test for scanning

--- rublox-parser/spec/rublox/parser_spec.rb
+++ rublox-parser/spec/rublox/parser_spec.rb
@@ -5,7 +5,38 @@ RSpec.describe Rublox::Parser do
     expect(Rublox::Parser::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  # TODO: Remove interpreter references from parser
+  class Interpreter
+    def self.error(line, message)
+      puts line, message
+    end
+  end
+
+  it "scans lexemes" do
+    source = <<~EOF
+    class Foo {
+      inFoo() {
+        print "in foo";
+      }
+    }
+
+    class Bar < Foo {
+      inBar(a, b) {
+        var a = 12.3;
+        var b = .23;
+        print nil;
+      }
+    }
+
+    class Baz < Bar {
+      inBaz() {
+        print 1+2 / 3;
+      }
+    }
+
+    var baz = Baz();
+    EOF
+    tokens = Rublox::Parser::Scanner.new(source).scan_tokens
+    expect(tokens.map(&:to_h)).to match_snapshot("scans_lexemes")
   end
 end
