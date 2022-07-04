cc258f0fd2f7e1ff3f99d83dc43cb272f62e5afd
Zach Thomae <zach@thomae.co>
2022-07-04 00:17:02 -0500

    Add rublox-bytecode gem

---
+++ rublox-bytecode/lib/rublox/bytecode.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "bytecode/version"
+
+module Rublox
+  module Bytecode
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ rublox-bytecode/spec/rublox/bytecode_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rublox::Bytecode do
+  it "has a version number" do
+    expect(Rublox::Bytecode::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
