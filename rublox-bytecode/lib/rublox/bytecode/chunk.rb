1940f3d19d76c85f91996cfa27a6bfb005cdba36
Zach Thomae <zach@thomae.co>
2022-07-04 01:01:14 -0500

    Add constants to bytecode

--- rublox-bytecode/lib/rublox/bytecode/chunk.rb
+++ rublox-bytecode/lib/rublox/bytecode/chunk.rb
@@ -3,18 +3,34 @@ module Rublox
     class Chunk
       def initialize
         @code = []
+        @constants = []
+        @lines = []
       end
 
       def count
         @code.length
       end
 
-      def write(byte)
+      def write(byte, line)
         @code << byte
+        @lines << line
+      end
+
+      def add_constant(value)
+        @constants << value
+        @constants.length - 1
       end
 
       def contents_at(offset)
         @code[offset]
+      end
+
+      def constant_at(offset)
+        @constants[offset]
+      end
+
+      def line_at(offset)
+        @lines[offset]
       end
     end
   end
--- rublox-bytecode/lib/rublox/bytecode/disassembler.rb
+++ rublox-bytecode/lib/rublox/bytecode/disassembler.rb
@@ -14,8 +14,17 @@ module Rublox
 
         def disassemble_instruction(chunk, offset)
           print "%04d " % offset
+          current_line = chunk.line_at(offset)
+          if offset > 0 && current_line == chunk.line_at(offset - 1)
+            print "   | "
+          else
+            print "%4d " % current_line
+          end
+
           instruction = chunk.contents_at(offset)
           case instruction
+          when Opcode::CONSTANT
+            return constant_instruction("OP_CONSTANT", chunk, offset)
           when Opcode::RETURN
             return simple_instruction("OP_RETURN", offset)
           else
@@ -25,6 +34,12 @@ module Rublox
         end
 
         private
+
+        def constant_instruction(name, chunk, offset)
+          constant = chunk.contents_at(offset + 1)
+          puts "%-16s %4d '%g'" % [name, constant, chunk.constant_at(constant)]
+          offset + 2
+        end
 
         def simple_instruction(name, offset)
           puts name
