023b1a13b49b340eb0ad1d8543b8629a200d493e
Zach Thomae <zach@thomae.co>
2022-07-03 21:59:20 -0500

    Move lox test cases into rublox-test library

---
+++ rublox-test/lib/rublox/test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "test/test_case"
+require_relative "test/suite"
+require_relative "test/collection"
+require_relative "test/version"
+
+module Rublox
+  module Test
+  end
+end
---
+++ rublox-test/spec/rublox/test_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rublox::Test do
+  it "has a version number" do
+    expect(Rublox::Test::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
